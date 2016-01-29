d13ab9b647cf1220c7fb5bd5a957ea1e7ef53b21
Christopher Queiroz <cferreira1987@gmail.com>
2016-01-29 12:12:14 -0200

    Initial commit

---
+++ js/projeto-me.js
@@ -0,0 +1,18 @@
+
+
+$(".inputCad").focus(function(){
+	$(this).css("border-bottom", "solid 2px #64b5f6");
+});
+
+$(".inputCad").blur(function(){
+
+
+		$(this).css("border-bottom", "solid 1px rgba(0,0,0,.5)")
+	
+
+});
+
+
+
+
+
